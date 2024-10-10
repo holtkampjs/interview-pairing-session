588661603a030ae427b7a522bc0fb43d4b91c6db
holtkampjs <holtkampjs@gmail.com>
2024-10-09 23:16:32 -0500

    WIP: Still not working. Lots of refactoring.
    Status: All tests are passing
    
    - Converted to poetry project
    - Added neighbors attribute
    - Added todos for making state machine

--- conway-game-of-life/src/game_of_life.py
+++ conway-game-of-life/src/game_of_life.py
@@ -1,4 +1,8 @@
+from collections import namedtuple
+from copy import deepcopy
 from enum import Enum
+
+Offset = namedtuple('Offset', ['row', 'col'])
 
 
 class Cell(Enum):
@@ -6,24 +10,80 @@ class Cell(Enum):
     ALIVE = 1
 
 
+# TODO: Create base class for initializing board
+# TODO: Create class for playing game state
+# TODO: Create class for static state
 class ConwaysGameOfLife:
     """An instance of Conway's Game of Life"""
+    _neighbors = None
+    _neighbor_index_offsets: tuple[Offset] = (
+        Offset(row=-1, col=-1),
+        Offset(row=-1, col=0),
+        Offset(row=-1, col=1),
+        Offset(row=0, col=-1),
+        Offset(row=0, col=1),
+        Offset(row=1, col=-1),
+        Offset(row=1, col=0),
+        Offset(row=1, col=1),
+    )
+
     def __init__(self, state: list[list[Cell]]):
         """Initializer"""
         self._state = state
         self._row_count = len(state)
         self._col_count = len(state[0])
 
-    def get_neighbor_count(self, row_idx: int, col_idx: int) -> list[list[Cell]]:
-        row_slice = slice(max(0, row_idx-1), row_idx+2)
-        col_slice = slice(max(0, col_idx-1), col_idx+2)
-        count_by_row = [
-            row[col_slice].count(Cell.ALIVE)
-            for row in
-            self._state[row_slice]
-        ]
-        count = sum(count_by_row) - self._state[row_idx][col_idx].value
-        return count
+    def _filter_out_of_bounds(self, coordinate: Offset) -> bool:
+        out_of_bounds: bool = (
+            coordinate.row < 0
+            or coordinate.col < 0
+            or coordinate.row > self._row_count-1
+            or coordinate.col > self._col_count-1
+        )
+        return not out_of_bounds
+
+    def _get_neigbor_count(self, row_idx: int, col_idx: int) -> int:
+        neighbor_indicies: tuple[Offset] = (
+            Offset(row=row_idx+offset.row, col=col_idx+offset.col)
+            for offset
+            in self._neighbor_index_offsets
+        )
+        neighbor_indicies: tuple[Offset] = tuple(
+            filter(self._filter_out_of_bounds, neighbor_indicies)
+        )
+        return sum([
+            self._state[index.row][index.col].value
+            for index in neighbor_indicies
+        ])
+
+    @property
+    def neighbors(self):
+        if not self._neighbors:
+            self._neighbors = [
+                [
+                    self._get_neigbor_count(row_idx, col_idx)
+                    for col_idx in range(len(self._state[row_idx]))
+                ]
+                for row_idx in range(len(self._state))
+            ]
+        return self._neighbors
+
+    # def _convert_neighbors_to_status(self, row: list[int]) -> list[Cell]:
+    #     pass
+
+    # def get_next_generation(self):
+    #     """Perform 1 cycle of the game"""
+    #     neigbor_map: list[list] = deepcopy(self.neighbors)
+    #     for row in x
+
+    #     for row_idx in range(self._row_count):
+    #         next_generation.append(list())
+    #         for col_idx in range(self._col_count):
+    #             next_generation[row_idx].append(Cell.DEAD)
+    #             neighbor_count = self.neighbors[row_idx][col_idx]
+    #             if neighbor_count == 2:
+    #                 next_generation[row_idx][col_idx] = self._state[row_idx][col_idx]
+    #     return next_generation
 
     def get_next_generation(self):
         """Perform 1 cycle of the game"""
@@ -32,7 +92,7 @@ class ConwaysGameOfLife:
             next_generation.append(list())
             for col_idx in range(self._col_count):
                 next_generation[row_idx].append(Cell.DEAD)
-                neighbor_count = self.get_neighbor_count(row_idx, col_idx)
+                neighbor_count = self.neighbors[row_idx][col_idx]
                 if neighbor_count == 2:
                     next_generation[row_idx][col_idx] = self._state[row_idx][col_idx]
         return next_generation
--- conway-game-of-life/test/test_game_of_life.py
+++ conway-game-of-life/test/test_game_of_life.py
@@ -29,3 +29,27 @@ def test_live_cell_with_two_live_neighbors():
     assert result[0][0] == Cell.DEAD, "expects cell 0, 0 to be dead"
     assert result[0][1] == Cell.ALIVE, "expects cell 0, 1 to be alive"
     assert result[0][2] == Cell.DEAD, "expects cell 0, 2 to be dead"
+
+
+def test_neighbor_count_trivial():
+    state = [[Cell.DEAD]]
+    game = ConwaysGameOfLife(state)
+    result = game.neighbors
+
+    assert result == [[0]], "should contain 1 cell indicating no neighbors"
+
+
+def test_neighbor_count_trivial_alive():
+    state = [[Cell.ALIVE]]
+    game = ConwaysGameOfLife(state)
+    result = game.neighbors
+
+    assert result == [[0]], "should contain 1 cell indicating no neighbors"
+    
+
+def test_neighbor_count_one():
+    state = [[Cell.ALIVE, Cell.DEAD]]
+    game = ConwaysGameOfLife(state)
+    result = game.neighbors
+
+    assert result == [[0, 1]], "should contain [0, 1] indicating 1 neighbor to cell 0, 1"
