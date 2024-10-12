55b649384c7566aa4476499e8c8cf5e601148bd0
Joseph Holtkamp <holtkampjs@gmail.com>
2024-10-12 14:34:07 -0500

    Working state. All rules are implemented and tested

--- conway-game-of-life/src/game_of_life.py
+++ conway-game-of-life/src/game_of_life.py
@@ -1,6 +1,70 @@
-class ConwaysGameOfLife:
-    def __init__(self):
-        pass
+from itertools import product
+from typing import Generator
 
-    def start(self):
-        pass
+
+class ConwaysGameOfLife:
+    def __init__(self, *, size: int = 0, alive: list[tuple[int, int]] = []):
+        # world is a square
+        self._size: int = size
+
+        # initialize all cells to zero (dead)
+        self._state: list[list[int]] = [
+            [0] * self._size for _ in range(self._size)
+        ]
+
+        # initialize neighbor count to zero
+        self._neighbors: list[list[int]] = [
+            [0] * self._size for _ in range(self._size)
+        ]
+
+        # populate alive cells and neighbor count
+        for row, col in alive:
+            self._state[row][col] = 1
+            for r, c in self._get_neighbors(row, col):
+                self._neighbors[r][c] += 1
+
+    def _get_neighbors(
+        self,
+        row: int,
+        col: int
+    ) -> Generator[tuple[int, int], None, None]:
+        for r, c in product(range(row-1, row+2), range(col-1, col+2)):
+            # skip if the cell for neighbor calculation
+            if r == row and c == col:
+                continue
+
+            # skip if out of bounds
+            if r < 0 or c < 0 or r >= self._size or c >= self._size:
+                continue
+
+            yield (r, c)
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @property
+    def state(self) -> list[list[int]]:
+        return self._state
+
+    @property
+    def neighbors(self) -> list[list[int]]:
+        return self._neighbors
+
+    # def _will_cell_be_alive(self, row: int, col: int) -> bool:
+    #     neighbor_count = self._neighbors[row][col]
+    #     if neighbor_count == 2 and self._state[row][col] == 1:
+    #         return True
+    #     if neighbor_count == 3:
+    #         return True
+
+    def get_next_generation(self):
+        alive_cells = []
+        for row, col in product(range(self._size), repeat=2):
+            neighbor_count = self._neighbors[row][col]
+            if (
+                neighbor_count == 3 or
+                (neighbor_count == 2 and self._state[row][col] == 1)
+            ):
+                alive_cells.append((row, col))
+        return ConwaysGameOfLife(size=self._size, alive=alive_cells)
--- conway-game-of-life/test/test_game_of_life.py
+++ conway-game-of-life/test/test_game_of_life.py
@@ -1,6 +1,127 @@
 from src.game_of_life import ConwaysGameOfLife
+from itertools import product
 
 
 def test_initialization():
     game = ConwaysGameOfLife()
     assert game
+
+
+def test_empty_create_world_three_by_three():
+    game = ConwaysGameOfLife(size=3)
+    assert game.size == 3
+    assert game.state == [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+
+
+def test_populate_world_three_by_three():
+    game = ConwaysGameOfLife(size=3, alive=[(0, 0)])
+    assert game.state == [
+        [1, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+
+
+def test_get_neighbors_three_by_three_population_of_one():
+    game = ConwaysGameOfLife(size=3, alive=[(0, 0)])
+    assert game.neighbors == [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 0, 0]
+    ]
+
+
+def test_get_neighbors_three_by_three_population_of_three():
+    game = ConwaysGameOfLife(size=3, alive=[(0, 0), (1, 1), (1, 2)])
+    assert game.neighbors == [
+        [1, 3, 2],
+        [2, 2, 1],
+        [1, 2, 2]
+    ]
+
+
+def test_underpopulation_one_lone_cell():
+    game = ConwaysGameOfLife(size=3, alive=[(0, 0)])
+    next_generation = game.get_next_generation()
+    assert next_generation.state == [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ], "All cells should be dead"
+
+
+def test_underpopulation_one_lone_cell_tiny_board():
+    game = ConwaysGameOfLife(size=1, alive=[(0, 0)])
+    next_generation = game.get_next_generation()
+    assert next_generation.state == [[0]], "All cells should be dead"
+
+
+def test_cell_living_on_with_two_neighbors():
+    game = ConwaysGameOfLife(
+        size=3,
+        alive=[(0, 0), (0, 1), (0, 2), (2, 0), (2, 1), (2, 2)]
+    )
+    next_generation = game.get_next_generation()
+    assert next_generation.state == [
+        [0, 1, 0],
+        [0, 0, 0],
+        [0, 1, 0]
+    ], "The cells at (0, 1) and (2, 1) should be alive"
+
+
+def test_cell_living_on_with_three_neighbors():
+    game = ConwaysGameOfLife(size=3, alive=[(0, 0), (0, 1), (1, 0), (1, 1)])
+    next_generation = game.get_next_generation()
+    assert next_generation.state == [
+        [1, 1, 0],
+        [1, 1, 0],
+        [0, 0, 0]
+    ]
+
+
+def test_overpopulation_cell_with_more_than_three_neighbors():
+    # Expected
+    #
+    # 1 1 1    1 1 1
+    # 0 1 0 -> 0 0 0
+    # 1 1 1    1 1 1
+    game = ConwaysGameOfLife(
+        size=3,
+        alive=[(0, 0), (0, 2), (2, 0), (2, 2), (1, 1), (0, 1), (2, 1)]
+    )
+    next_generation = game.get_next_generation()
+    assert next_generation.state == [
+        [1, 1, 1],
+        [0, 0, 0],
+        [1, 1, 1]
+    ]
+
+
+def test_overpopulation_fully_populated():
+    game = ConwaysGameOfLife(
+        size=3,
+        alive=list(product(range(3), repeat=2))
+    )
+    next_generation = game.get_next_generation()
+    assert next_generation.state == [
+        [1, 0, 1],
+        [0, 0, 0],
+        [1, 0, 1]
+    ]
+
+
+def test_reproduction_with_one_cell_in_three_corners_of_three_by_three():
+    game = ConwaysGameOfLife(
+        size=3,
+        alive=[(0, 0), (0, 2), (2, 0)]
+    )
+    next_generation = game.get_next_generation()
+    assert next_generation.state == [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ], "Center cell should be produced, others should perish"
