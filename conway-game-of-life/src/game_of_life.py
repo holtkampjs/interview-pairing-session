007460575c860f5f33641c769127664c9cdf8de1
Joseph Holtkamp <holtkampjs@gmail.com>
2024-10-13 15:23:44 -0500

    Added typing and refactored

--- conway-game-of-life/src/game_of_life.py
+++ conway-game-of-life/src/game_of_life.py
@@ -1,57 +1,51 @@
 from itertools import product
-from typing import Generator
+from typing import TypeAlias
+
+Point: TypeAlias = tuple[int, int]
+Grid: TypeAlias = list[list[int]]
 
 
 class ConwaysGameOfLife:
-    def __init__(self, *, size: int = 0, alive: list[tuple[int, int]] = []):
+    def __init__(self, *, size: int = 0, alive: list[Point] = []):
         # world is a square
         self._size: int = size
 
         # initialize all cells to zero (dead)
-        self._state: list[list[int]] = [
+        self._state: Grid = [
             [0] * self._size for _ in range(self._size)
         ]
 
         # initialize neighbor count to zero
-        self._neighbors: list[list[int]] = [
+        self._neighbors: Grid = [
             [0] * self._size for _ in range(self._size)
         ]
 
-        # populate alive cells and neighbor count
         for row, col in alive:
+            # populate alive cell
             self._state[row][col] = 1
-            for r, c in self._get_neighbors(row, col):
+
+            # increment neighbor counts
+            for r, c in product(range(row-1, row+2), range(col-1, col+2)):
+                # skip if out of bounds
+                if r < 0 or c < 0 or r >= self._size or c >= self._size:
+                    continue
+                if r == row and c == col:
+                    continue
                 self._neighbors[r][c] += 1
-
-    def _get_neighbors(
-        self,
-        row: int,
-        col: int
-    ) -> Generator[tuple[int, int], None, None]:
-        for r, c in product(range(row-1, row+2), range(col-1, col+2)):
-            # skip if the cell for neighbor calculation
-            if r == row and c == col:
-                continue
-
-            # skip if out of bounds
-            if r < 0 or c < 0 or r >= self._size or c >= self._size:
-                continue
-
-            yield (r, c)
 
     @property
     def size(self) -> int:
         return self._size
 
     @property
-    def state(self) -> list[list[int]]:
+    def state(self) -> Grid:
         return self._state
 
     @property
-    def neighbors(self) -> list[list[int]]:
+    def neighbors(self) -> Grid:
         return self._neighbors
 
-    def _will_cell_be_alive(self, index: tuple[int, int]) -> bool:
+    def _will_cell_be_alive(self, index: Point) -> bool:
         row, col = index
         neighbor_count = self._neighbors[row][col]
         if neighbor_count == 2 and self._state[row][col] == 1:
